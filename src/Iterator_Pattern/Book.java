15e3c46ff601e8628965bbb8afc9eb8263ecf124
山口絃 <gen.yamaguchi@exc.tokai-rika.co.jp>
2023-12-25 18:46:17 +0900

    Iterator_Pattern

---
+++ src/Iterator_Pattern/Book.java
@@ -0,0 +1,14 @@
+package Iterator_Pattern;
+
+
+public class Book {
+    private String name;
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
